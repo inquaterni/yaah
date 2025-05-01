57e8bdd5061249172d63ec256c47b2d6f99cb1ef
inquaterni <m.matzkevich@gmail.com>
2025-05-01 17:44:40 +0300

    trying to fix CI #2

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# If the build user doesn't exist, create it and re-exec the script under that user
+if [ "$(id -u)" -eq 0 ]; then
+  useradd --create-home builder
+  chown -R builder:builder "$(pwd)"
+  exec su builder -c "$0"
+fi
+
 makepkg --printsrcinfo >.SRCINFO
-makepkg --asroot --noconfirm -si
+makepkg --noconfirm -si
